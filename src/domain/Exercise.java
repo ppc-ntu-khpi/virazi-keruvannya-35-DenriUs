40946c15053bc97469b9cb39b854962a82edb5e7
DenriUs <den.tacij@gmail.com>
2020-04-24 16:44:57 +0300

    Add functionality to find symmetric time count

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,30 @@
 package domain;
+ 
+public class Exercise { 
+    public static int Calculate(Boolean showExecutionLog) {
+        int[] time = new int[2];
+        int symmetricTimeCount = 0;
 
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
-}
+        for (int i = 0; i <= 2; i++) {
+            time[0] = i;
+
+            for (int j = 0; j <= 9; j++) {
+                time[1] = j;
+
+                if (time[1] < 6) {
+                    if (time[0] == 2 && time[1] >= 4) {
+                        break;
+                    }
+
+                    symmetricTimeCount++;
+
+                    if (showExecutionLog) {
+                        System.out.println("(" + symmetricTimeCount + ")" + " Symmetric time: " + time[0] + time[1] + ":" + time[1] + time [0]);
+                    }
+                }
+            }
+        }
+
+        return symmetricTimeCount;
+    } 
+} 
